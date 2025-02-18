78c0a6300ccf3e4e1eccc13f53310d1a7290ca94
Gabriel <gabrielescodino@gmail.com>
2025-02-17 21:48:29 -0300

    Rename envs

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tcl-terraform-bucket-vitorrafael"
+    bucket = "tcl-terraform-bucket-apresentacao"
     key    = "soat8-g6/api-docs/terraform.tfstate"
     region = "us-east-1"
   }
